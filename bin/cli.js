e7ab0fceff1615c36aad0b936b016bc9a1ede91f
hystolytc <melife4@gmail.com>
2021-08-19 17:07:30 +0700

    update script

--- bin/cli.js
+++ bin/cli.js
@@ -9,24 +9,20 @@ const runCmd = cmd => {
     console.error(`Failed to execute ${cmd}`, ex)
     return false
   }
+
   return true
 }
 
 const repoName = process.argv[2]
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/hystolytc/create-app-react ${repoName}`
 const installDepsCommand = `cd ${repoName} && npm install`
-const changeDirectoryCommand = `cd ${repoName}`
 
 console.log(`Scaffold the repository with name ${repoName}`)
 const checkedOut = runCmd(gitCheckoutCommand)
-if (checkedOut) process.exit(-1)
+if (!checkedOut) process.exit(-1)
 
 console.log(`Installing dependency for ${repoName}`)
 const installedDeps = runCmd(installDepsCommand)
 if (!installedDeps) process.env(-1)
-
-console.log(`Change directory to ${repoName}`)
-const changedDirectory = runCmd(changeDirectoryCommand)
-if (!changedDirectory) process.exit(-1)
 
 console.log('Yeay all setup is success, let\'s try it with typing this command npm dev')
